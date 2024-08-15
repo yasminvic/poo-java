9a8092fe4ae34a0bedd98ed57a9555464bff6307
Yasmin Victória Alves De Souza <yvasouza@furb.br>
2024-08-14 21:33:57 -0300

    Criação da entidade

---
+++ SistemaBancario/src/Entity/ContaBancaria.java
@@ -0,0 +1,31 @@
+package Entity;
+
+public class ContaBancaria {
+    private String titular;
+    private String numero;
+    private double saldo;
+
+    //GETTERS e SETTERS
+    public String getTitular() {
+        return titular;
+    }
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+    public void depositar(double valor){
+        
+    }
+}
